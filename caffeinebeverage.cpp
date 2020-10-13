fc7c8a8f57ab76ea4dba4c6c5ed47956713e5d21
Mark <fletinrok@gmail.com>
2020-10-13 12:17:56 +0700

    добавить класс CaffeineBeverage

---
+++ caffeinebeverage.cpp
@@ -0,0 +1,13 @@
+#include "caffeinebeverage.h"
+
+CaffeineBeverage::CaffeineBeverage()
+{
+
+}
+
+void CaffeineBeverage:: boilWater() {
+    cout <<"Boiling water" << endl;
+}
+void CaffeineBeverage:: pourInCup() {
+    cout <<"Pouring into cup" << endl;
+}
---
+++ caffeinebeverage.h
@@ -0,0 +1,17 @@
+#ifndef CAFFEINEBEVERAGE_H
+#define CAFFEINEBEVERAGE_H
+
+#include <iostream>
+
+using namespace std;
+
+class CaffeineBeverage
+{
+public:
+    CaffeineBeverage();
+    virtual void prepareRecipe() = 0;
+    void boilWater();
+    void pourInCup();
+};
+
+#endif // CAFFEINEBEVERAGE_H
